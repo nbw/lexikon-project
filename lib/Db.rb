c1cd5e7d0e7ac8ac2effa415121e31f211e7e6d9
sabasis <willson.b.nathan@gmail.com>
2013-10-17 00:16:55 -0700

    ignore situation sorted

--- lib/Db.rb
+++ lib/Db.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'lib/serverconfig'
+
 module Db
 
 
--- lib/example_serverconfig.rb
+++ lib/example_serverconfig.rb
@@ -1,6 +1,9 @@
 #config file with postgres server variables
+#
+# 1. fill in your local settings
+# 2. rename to "serverconfig.rb"
+#
 HOST = "localhost"
 PORT = 5432
 DBNAME = "dictproject"
 TNAME = 'dict'
-
